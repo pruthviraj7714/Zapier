18b514c2a4009220205d1ea2cb4e65db3e31ea3d
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-07-26 15:12:42 +0530

    added processor microservice

---
+++ apps/processor/src/index.ts
@@ -0,0 +1,32 @@
+import db from "@repo/db/client";
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+export const TOPIC_NAME = "zap-events";
+
+const main = async () => {
+  const producer = kafka.producer();
+  await producer.connect();
+
+  while (1) {
+    const pendingRows = await db.zapRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((r) => {
+        return {
+          value: r.zapRunId,
+        };
+      }),
+    });
+  }
+};
+
+main();
