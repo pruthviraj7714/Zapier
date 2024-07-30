83e5c2b850b3ae924a3b7bda9cc4f09425144ec5
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-07-30 16:29:23 +0530

    added welcome message to users

--- apps/client/src/app/dashboard/page.tsx
+++ apps/client/src/app/dashboard/page.tsx
@@ -10,13 +10,23 @@ export default function Dashboard() {
   const router = useRouter();
   const [zaps, setZaps] = useState([]);
   const [deleting, setDeleting] = useState<string | null>(null);
+  const [name, setName] = useState();
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(true);
 
   const getZaps = async () => {
-    const res = await axios.get("/api/zap/all-zaps");
-    setZaps(res.data.zaps);
+    try {
+      const res = await axios.get("/api/zap/all-zaps");
+      setZaps(res.data.zaps);
+    } catch (error: any) {
+      console.log(error.message);
+      toast({
+        title: error.response?.data?.message ?? "Error while fetching zaps!",
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
-
-  const { toast } = useToast();
 
   const handleDelete = async (id: string) => {
     try {
@@ -29,20 +39,39 @@ export default function Dashboard() {
       toast({
         title: error.response.data.message,
       });
-    }finally {
+    } finally {
       setDeleting(null);
     }
-    
   };
 
+  const userInfo = async () => {
+    try {
+      const res = await axios.get('/api/user/info');
+      setName(res.data.user.name);
+    } catch (error : any) {
+      toast({
+        title : error.response.data.message ?? "Error while fetching User info!"
+      })
+    }
+  }
 
   useEffect(() => {
+    userInfo();
     getZaps();
   }, [deleting]);
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center text-green-500 text-4xl font-serif min-h-screen">
+        Loading..
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen flex flex-col p-6 bg-gray-100">
-      <div className="flex justify-center">
+      <div className="my-6 font-serif text-5xl">Welcome to Zapier, {name}!</div>
+      <div className="flex justify-center my-6">
         <Button
           className="bg-blue-500 text-white font-bold py-2 px-5 rounded-full shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-1"
           onClick={() => {
@@ -57,7 +86,7 @@ export default function Dashboard() {
           My Zaps
         </h1>
         {zaps && zaps.length > 0 ? (
-          <ZapTable zaps={zaps}  onDelete={handleDelete} />
+          <ZapTable zaps={zaps} onDelete={handleDelete} />
         ) : (
           <div className="flex justify-center text-5xl font-bold text-slate-600">
             No Zaps Created
