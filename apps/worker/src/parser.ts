279998359102d6dd832ca05d78a7fa030bcc8463
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-01 22:15:01 +0530

    added string parser function

---
+++ apps/worker/src/parser.ts
@@ -0,0 +1,41 @@
+export function parse(
+  text: string,
+  values: any,
+  startDelimiter = "{",
+  endDelimiter = "}"
+) {
+  let finalString = "";
+  let i = 0;
+
+  if(!text) return ""
+
+  while (i < text.length) {
+    if (text[i] === startDelimiter) {
+      let endPoint = i + 1;
+      while (text[endPoint] !== endDelimiter && endPoint < text.length) {
+        endPoint++;
+      }
+      if (text[endPoint] === endDelimiter) {
+        const keys = text.slice(i + 1, endPoint).split(".");
+        let localValues = values;
+        for (const key of keys) {
+          localValues = localValues[key];
+          if (localValues === undefined) break;
+        }
+        finalString +=
+          localValues !== undefined
+            ? localValues
+            : `${startDelimiter}${keys.join(".")}${endDelimiter}`;
+        i = endPoint + 1;
+      } else {
+        finalString += text[i];
+        i++;
+      }
+    } else {
+      finalString += text[i];
+      i++;
+    }
+  }
+
+  return finalString;
+}
