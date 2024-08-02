273c76e485595e439681ae43d1480cc00f115788
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-02 16:46:37 +0530

    added theme

--- apps/client/src/components/Providers.tsx
+++ apps/client/src/components/Providers.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-
+import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </SessionProvider>
     </>
-  )
-
+  );
 }
